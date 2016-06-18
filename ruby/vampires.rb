929d7bc4d8d3a931674fe4f07fcd89060b551e23
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-06-18 13:53:35 -0400

    Them bloody glittering vampires

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,61 @@
-puts "What is your name?"
-name=gets.chomp
-puts "How old are you?"
-age=gets.chomp.to_i
-puts "What year were you born?"
-birth=gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic=gets.chomp
-puts "Would you lie to enroll in the company's health insurance?"
-insurance=gets.chomp
+puts "How many employees will be interviewed?"
+i=gets.chomp.to_i;
+a=0;
 
+while a<i do
+puts "What is your name?"
+name=gets.chomp.downcase;
+puts "How old are you?"
+age=gets.chomp.to_i;
+puts "What year were you born?"
+birth=gets.chomp.to_i;
+year=2016;
+Age=year-birth;
+if Age == age
+	correct_age = true;
+else
+	correct_age = false;	
+end
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic=gets.chomp.downcase;
+if garlic != "yes"
+	likes_garlic = false;
+else
+	likes_garlic = true;
+end
+puts "Would you like to enroll in the company's health insurance?"
+insurance=gets.chomp.downcase;
+if insurance != "yes"
+	wants_insurance = false;
+else
+	wants_insurance = true;
+end
+
+puts "Please list up to 10 allergies."
+allergy=gets.chomp.downcase
+allergy_count = 0;
+until allergy_count == 10 || allergy == "none" do
+	if allergy == "sunshine"
+		puts "Probably a vampire."
+		break
+	end
+	allergy = gets.chomp.downcase;
+	allergy_count +=1;
+	break
+end
+
+if name == "drake cula" || name == "tu fang"
+	puts "Definitely a vampire."
+elsif correct_age && (likes_garlic || wants_insurance) == true
+	puts "Probably not a vampire."
+	elsif correct_age && (likes_garlic || wants_insurance) == false
+	puts "Probably a vampire."
+elsif correct_age && likes_garlic && wants_insurance == false
+	puts "Almost certainly a vampire."
+else
+	puts "Results inconclusive."
+end		
+a +=1;
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all Just Be Friends."
