40dec7a98c89a6309fe2b8b4b6da39b9db012341
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-06-21 22:59:18 -0400

    Add p to beginning of call to display result

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,38 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-"zom".insert(1, 'o')
+p "zom".insert(1, 'o')
 # => “zoom”
 
-"enhance".center(15)
+p "enhance".center(15)
 # => "    enhance    "
 
-"Stop! You’re under arrest!".upcase
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-"the usual".insert(-1, " suspects")
+p "the usual".insert(-1, " suspects")
 #=> "the usual suspects"
 
-" suspects".insert(0, "the usual")
+p " suspects".insert(0, "the usual")
 # => "the usual suspects"
 
-"The case of the disappearing last letter".chop
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".delete 'T'
+p "The mystery of the missing first letter".delete 'T'
 # => "he mystery of the missing first letter"
 
-"Elementary,    my   dear        Watson!".squeeze(" ")
+p "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-"z".ord
+p "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 # It is the interger ordinal of z
 
-"How many times does the letter 'a' appear in this string?".count "a"
+p "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
 
