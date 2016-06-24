07cf5a3a35673e575efb972db4a88d5cb2d0ac3d
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-06-23 22:57:08 -0400

    Add commented reflection for Release 5

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -92,6 +92,18 @@ p grocery_list
 display_list(grocery_list)
 
 # 1. Well, I don't really like it. I'm more of a person who jumps right into coding. I do, however understand the value of it.
-# It's basically creating a skeleton of the entire program, of what you want it to do and what it should do.
+# It's basically creating a skeleton of the entire program, of what you want it to do and what it should do. This will also limit unnecessary lines of codes.
+# And yes, it is something I should really work on knowing how to do. :'|
 
-#2. 
+#2. The tradeoff of using arrays and hashes in this challenge is that while arrays are helpful in creating lists, the objects in the array cannot have a value other than itself.
+# Hashes, on the other hand, can have objects that have a different value. However, it cannot turn a string of words into each individual key. That's why it's a necessity to use
+# both arrays and hashes for this challenge.
+
+#3. methods explicitly returns the last line of code written in the method definition. Implicity return or using the "return" would return the value in which the method obtains, or is set out to calculate.
+
+#4. Suprisingly, you can pass a lot of things as an argument. Integers, variables, arrays, strings, even hashes.
+
+#5. Since almost anything can be used as an argument for the methods, information can be shared between them if the argument used is an object in which the previous method's value(s) are stored in.
+
+#6. Before I thought it was impossible to have nested methods, however, today I learned that that's not the case. It's totally possible to have defined method while defining a method. It's just that defining a method
+# while inside a method being defined, is really hard to do.
