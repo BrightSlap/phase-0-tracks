2ad9fbcc5c4d172e5346d0eef206b71d7e0d4d3f
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-07-06 23:09:34 -0400

    Design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES-------------
+# Shetland Sheepdog
+
+# CHARACTERISTICS-----
+# Name: Trevor
+# Color: White and Brown
+# Fur: The softest
+# Friendly: Yes
+
+# BEHAVIOR------------
+# Roll Over
+# Bark
+# Paw
+# High Five
