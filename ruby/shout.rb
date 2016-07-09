a124a799196c9cb85c93f887ac23c46761cf1635
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-07-08 22:08:19 -0400

    Convert old module to mixin module with drunkard and player class

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,33 @@
-module Shout
-  # we'll put some methods here soon, but this code is fine for now!
-   def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yell_happily(words)
-  	words + "!!" + " :D"
-  end
+# module Shout
+#   # we'll put some methods here soon, but this code is fine for now!
+#    def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yell_happily(words)
+#   	words + "!!" + " :D"
+#   end
 
-end
+# end
 
 # Shout.yell_angrily("Curses")
 # Shout.yell_happily("Nice")
 
+module Shout
+	def round(subject)
+		puts "Let's get in one more round of #{subject}!"
+	end
+end
+
+class Drunkard
+	include Shout
+end
+
+class Player
+	include Shout
+end
+
+drunkard = Drunkard.new
+drunkard.round("whiskey")
+
+player = Player.new
+player.round("paintball")
