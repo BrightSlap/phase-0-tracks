a4a748995438ea0eda2299b43451876b1933366a
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-07-08 21:51:32 -0400

    make loop for random attributes of santas.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -13,11 +13,30 @@
 # end
 
 
+# define santa class
+# method speak
+# print "ho,ho,ho! haaappy holidays!"
+# end
+
+# eat_milk_and_cookies(cookie_type)
+# @cookie_type = cookie_type
+# prints "That was a good #{cookie_type}!"
+# end
+
+# initialize
+# prints "Initializing Santa..."
+# end
+
+
 class Santa
+	
+	attr_reader :raindeer_rank, :ethnicity
+	attr_accessor :gender, :age
 	
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
+		puts "This Santa is a #{@gender} #{@ethnicity}"
 	end
 
 	def speak
@@ -28,20 +47,37 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 	
-	def raindeer_ranking
-		raindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	def age(number)
+		age = number
+		@age = age
+		puts "This santa is #{@age} years old."
 	end
-	
-	def age
-		@age = 0
+
+	def get_mad_at(rank, raindeer_name)
+		raindeer_rank = rank
+		raindeer_rank.delete(raindeer_name)
+		raindeer_rank << raindeer_name
+		@raindeer_rank = raindeer_rank
+		puts "This santa's raindeer ranking goes: #{@raindeer_rank}"
 	end
 end
 
-santas = []
-example_genders = ["female", "bigender", "male", "N/A"]
-example_ethnicities = ["latino", "white", "asian", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+# 
+# p santa = Santa.new("attack helicopter", "metal")
+# p santa.get_mad_at(raindeer, "Rudolph")
+# p santa.get_mad_at(raindeer, "Blitzen")
+# p santa.gender = "female"
 
-p santas
+
+
+example_genders = ["female", "bigender", "male", "attack helicopter", "N/A"]
+example_ethnicities = ["latino", "white", "asian", "metal", "N/A"]
+raindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"] 
+50.times do |i|
+	new_genders = example_genders.shuffle
+	new_ethnicities = example_ethnicities.shuffle
+  	santa = Santa.new(new_genders[1], new_ethnicities[1])
+	random_raindeer = raindeer.shuffle
+	santa.get_mad_at(random_raindeer, random_raindeer[1])
+	santa.age(rand(140))
+end
---
+++ ruby/solo.rb
@@ -0,0 +1,4 @@
+class dragon
+	age
+	type = [wyrm, wyvern, drake, serpent]
+	color = [red, black, green bronze, gold, silver]
