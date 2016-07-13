be33db9498276efa400efe345dd209363b958237
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-07-12 21:09:19 -0400

    psuedocode for finding longest phrase

---
+++ js/algos.js
@@ -0,0 +1,5 @@
+// create funtion
+// function takes in array
+// array is fulled of words/phrases
+// function runs through the array to check for words/phrases with longest length
+// return index with longest length
