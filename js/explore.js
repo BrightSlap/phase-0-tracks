3baeb7c5c92457afdc0247b35cf42bdcb7a0fa07
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-07-12 21:34:37 -0400

    create function to reverse string. store function result as variable. display variable using conditional statements.

--- js/explore.js
+++ js/explore.js
@@ -1,4 +1,24 @@
 // create function
-// 	call function reverse() with "hello" as its token
+// call function reverse() with "hello" as its token
+//	set variable "reversed" is set as empty
+//	for loop of string length
+//		variable of i is set as string length - 1; as i is >= to 0; i--
+//		reversed is equal to reversed + i;
+//	end loop
+//	console.log(reversed);
+//end function
 
-	
+function reverse(string) {
+  var reversed = '';
+  for (var i = string.length - 1; i >= 0; i--) {
+  	reversed = reversed + string[i];
+  }
+  console.log(reversed);
+  return reversed;
+}
+reverse("hello");
+var longestWord = reverse("pneumonoultramicroscopicsilicovolcanoconiosis");
+
+if (1 == 1) {
+	console.log(longestWord);
+}
