e1cb889dd7323e779916c267743972fad9ff085e
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-07-14 22:33:11 -0400

    made function longestphrase DRY

--- js/algos.js
+++ js/algos.js
@@ -15,18 +15,31 @@ function longestphrase(array) {
 	var longest = '';
 
 	for(var i = 0; i < array.length-1; i++) {
- 		var longphrase1 = array[i];
- 		var longphrase2 = array[i+1];
- 		if ((longphrase1.length > longphrase2.length) && (longphrase1.length > longest.length)) {
- 			longest = longphrase1;
- 		} else if ((longphrase1.length < longphrase2.length) && (longphrase2.length > longest.length)) {
- 			longest = longphrase2;
+ 		if ((array[i].length > array[i+1].length) && (array[i].length > longest.length)) {
+ 			longest = array[i];
+ 		} else if ((array[i].length < array[i+1].length) && (array[i+1].length > longest.length)) {
+ 			longest = array[i+1];
  		} else {
  			longest = longest;
  		}
  	}
  	console.log("The longest phrase in the array is: " + longest);
  	return longest;
+}
+
+//	create function
+//	function takes two hashes
+//		if/else compare first two keys of the hashes
+//		if true, print "the first key of each hash have matching values"
+//		else compare hash1 first key with hash2 2nd key
+//		if true, print "the first key of the first hash and the second key of the second hash have matching values"
+//		keep going through all the keys of both hashes.
+//		if none of the keys match, print "the keys of the first hash has no matching values with the keys of the second hash."
+//	end
+
+function key-value_match(hash1, hash2) {
+	
+	
 }
 
 longestphrase(["God save the Queen!", "Catchphrase!", "Bad day, bad day, bad day!"]);
