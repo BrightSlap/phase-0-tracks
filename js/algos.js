baaea42e60e2ad657943cb12fb0c8867faec4007
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-07-15 02:30:13 -0400

    Create function to compare hashes for matching values. Create function for randome number generator and creating arrays of random lenght with strings of random length.

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,4 @@
-// create funtion
+ // create funtion
 // function takes in array
 // array is fulled of words/phrases
 // function runs through the array to check for words/phrases with longest length
@@ -13,7 +13,6 @@
 
 function longestphrase(array) {
 	var longest = '';
-
 	for(var i = 0; i < array.length-1; i++) {
  		if ((array[i].length > array[i+1].length) && (array[i].length > longest.length)) {
  			longest = array[i];
@@ -23,24 +22,92 @@ function longestphrase(array) {
  			longest = longest;
  		}
  	}
- 	console.log("The longest phrase in the array is: " + longest);
+ 	console.log("The longest phrase in this array is: " + longest);
  	return longest;
 }
 
 //	create function
 //	function takes two hashes
-//		if/else compare first two keys of the hashes
-//		if true, print "the first key of each hash have matching values"
-//		else compare hash1 first key with hash2 2nd key
-//		if true, print "the first key of the first hash and the second key of the second hash have matching values"
-//		keep going through all the keys of both hashes.
-//		if none of the keys match, print "the keys of the first hash has no matching values with the keys of the second hash."
+//		declare variable match as 0, for counter of how many matching values
+//		for loop of the keys in hash1
+//			nested for loop of keys in hash2
+//				if compare first key of hash1 to all keys of hash2
+//					if true, add 1 to match counter
+//			keep going through all the keys of both hashes.
+//		if match counter is greater than or equal to 1,
+//			declare variable result as true
+//		else
+//			declare variable result as false
+//		print result
 //	end
 
-function key-value_match(hash1, hash2) {
-	
-	
+function keyvaluematch(hash1, hash2) {
+	var match = 0;
+	for(var key in hash1) {
+		for(var prop in hash2) {
+			if (hash1[key] == hash2[prop]) {
+				match = match + 1
+			}
+		}
+	}
+	if (match >= 1) {
+		var result = true;	}
+	else {
+		var result = false; }
+	console.log(result);
 }
+
+//	create function for random integer generator(RIG)
+//		Math.random generates a number between 0 and 1(excluding)
+//		Math.floor rounds down the number to the closest integer
+//		to include the max number multiply the number by the result of max - min +1 and add the min to that product
+
+
+//	create function that takes interger as arguement
+//		declare variable array as empty array
+//		for loop declare variable index as 0, as index is smaller than arguement, index++
+//			declare variable alphabet as alphabet
+//			declare variable strlength as RIG to set random string length
+//			function for creating random word (rndWord)
+//				declare variable rndString as an empty string
+//				for loop declare another variable index as 0, as index is smaller than strlength, index++
+//					rndString is set as rndString + alphabet[RIG]
+//				return rndString				
+//			array.push rndWord
+//		print array
+
+function rig(min, max) {
+  			return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomArrays(integer) {
+	var array = []
+	for (var i = 0; i < integer; i++) {
+		var alphabet = "abcdefghijklmnopqrstuvwxyz";
+		var strlength = rig(1, 10);
+		function rndWord() {
+			var rndString = ''
+			for (var i = 0; i < strlength; i++) {
+				rndString = rndString + alphabet[rig(0, 25)];
+			}
+			return rndString
+		}
+		array.push (rndWord());	
+	}
+	console.log(array);
+	return array;
+}
+
 
 longestphrase(["God save the Queen!", "Catchphrase!", "Bad day, bad day, bad day!"]);
 longestphrase(["God save the Queen!", "Catchphrase!", "Just do it!", "Supercalifragilisticexpialidocious"]);
+
+keyvaluematch({name: "Steven", age: 54}, {name: "Steven", age: 34})
+keyvaluematch({name: "Tamir", age: 34}, {name: "Steven", age: 54})
+
+randomArrays(5)
+randomArrays(3)
+
+for(var i =1; i <= 10; i++) {
+	longestphrase(randomArrays(rig(3,6)))
+}
