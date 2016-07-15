289e443fa44947d6f8a9b31e2f311eda2ce81098
Arthur Chiu <Arthur.Chiu.6169@gmail.com>
2016-07-14 22:10:09 -0400

    Create constructor function for Car object.

--- js/data_structures.js
+++ js/data_structures.js
@@ -21,3 +21,26 @@ for (var i = 0; i < names.length; i++) {
 }
 
 console.log(h_c);
+
+function Car(manufacturer, color, hasTurbo) {
+	console.log("Our new car:", this);
+	this.manufacturer = manufacturer;
+	this.color = color;
+	this.hasTurbo = hasTurbo;
+	this.revEngine = function() { console.log("VROOM! *Blows out valve...* :'("); };
+	console.log("Car Initialization Completed.");
+}
+
+console.log("Let's buy a new car");
+var freshRide = new Car("RiceRocket", "White", true);
+console.log(freshRide);
+console.log("Wanna race?");
+freshRide.revEngine();
+
+console.log("----------------");
+
+console.log("Time to buy another new car");
+var familyRide = new Car("AllFitsOne", "Purple", false);
+console.log(familyRide);
+console.log("Pick up kids at 3? *Clock read 2:58*");
+familyRide.revEngine();
